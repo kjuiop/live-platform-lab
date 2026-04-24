714bc32703f044e69015b14b406b72794be48214
kjuiop <copying_y@naver.com>
2026-04-24 15:15:09 +0900

    [#63] refactor: scheduler config 를 별도 파일로 추출

---
+++ live-chat-server/src/main/java/org/giglab/live/config/SchedulingConfig.java
@@ -0,0 +1,8 @@
+package org.giglab.live.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@Configuration
+public class SchedulingConfig {}

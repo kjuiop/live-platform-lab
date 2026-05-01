3a249c54c4ed6a1d5878aa4b126712ecf6860209
kjuiop <copying_y@naver.com>
2026-05-01 19:45:56 +0900

    [#88] refactor: mergetree 를 replacing merge tree 로 변경

--- config/clickhouse/init.sql
+++ config/clickhouse/init.sql
@@ -2,13 +2,14 @@ CREATE DATABASE IF NOT EXISTS analytics;
 
 CREATE TABLE IF NOT EXISTS analytics.events
 (
-    event_id     String,
-    event_type   LowCardinality(String),
-    occurred_at  DateTime64(3, 'UTC'),
-    session_id   String,
-    user_id      Nullable(String),
-    room_id      String,
-    properties   String
-) ENGINE = MergeTree()
+    event_id       String,
+    event_type     LowCardinality(String),
+    occurred_at    DateTime64(3, 'UTC'),
+    occurred_at_ms UInt64,
+    session_id     String,
+    user_id        Nullable(String),
+    room_id        String,
+    properties     String
+) ENGINE = ReplacingMergeTree(occurred_at_ms)
 PARTITION BY toYYYYMM(occurred_at)
-ORDER BY (room_id, event_type, occurred_at);
+ORDER BY (room_id, event_type, occurred_at, event_id);

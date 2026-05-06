91bfe0ca997311b33718677432c39e5fbff3d8a7
kjuiop <copying_y@naver.com>
2026-05-06 17:20:02 +0900

    [#108] feat: metric naming add

--- live-chat-server/src/main/java/org/giglab/live/global/config/AsyncConfig.java
+++ live-chat-server/src/main/java/org/giglab/live/global/config/AsyncConfig.java
@@ -1,6 +1,8 @@
 package org.giglab.live.global.config;
 
-import java.util.concurrent.Executor;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.binder.jvm.ExecutorServiceMetrics;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,13 +12,17 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @Configuration
 public class AsyncConfig {
 
-  @Bean(name = "faqAsyncExecutor")
-  public Executor faqAsyncExecutor() {
-    return Executors.newVirtualThreadPerTaskExecutor();
+  @Bean(name = "faqAsyncExecutor", destroyMethod = "shutdown")
+  public ExecutorService faqAsyncExecutor(MeterRegistry meterRegistry) {
+    var executor =
+        Executors.newThreadPerTaskExecutor(Thread.ofVirtual().name("faq-async-", 0).factory());
+    return ExecutorServiceMetrics.monitor(meterRegistry, executor, "faq_async_executor");
   }
 
-  @Bean(name = "chatAsyncExecutor")
-  public Executor chatAsyncExecutor() {
-    return Executors.newVirtualThreadPerTaskExecutor();
+  @Bean(name = "chatAsyncExecutor", destroyMethod = "shutdown")
+  public ExecutorService chatAsyncExecutor(MeterRegistry meterRegistry) {
+    var executor =
+        Executors.newThreadPerTaskExecutor(Thread.ofVirtual().name("chat-async-", 0).factory());
+    return ExecutorServiceMetrics.monitor(meterRegistry, executor, "chat_async_executor");
   }
 }
